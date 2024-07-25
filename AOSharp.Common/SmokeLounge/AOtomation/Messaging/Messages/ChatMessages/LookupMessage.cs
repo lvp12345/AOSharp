1d0147aa0535c1c64154e60de9c94f53a2594e21
Tyler Delmus <tylerdelmus@gmail.com>
2024-07-25 09:14:16 -0500

    Added chat LookupMessage

---
+++ AOSharp.Common/SmokeLounge/AOtomation/Messaging/Messages/ChatMessages/LookupMessage.cs
@@ -0,0 +1,24 @@
+﻿namespace SmokeLounge.AOtomation.Messaging.Messages.ChatMessages
+{
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+    using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+    [AoContract((int)ChatMessageType.LookupMessage)]
+    public class LookupMessage : ChatMessageBody
+    {
+        #region Public Properties
+
+        public override ChatMessageType PacketType
+        {
+            get
+            {
+                return ChatMessageType.LookupMessage;
+            }
+        }
+
+        #endregion
+
+        [AoMember(0, SerializeSize = ArraySizeType.Int16)]
+        public string Name { get; set; }
+    }
+}

38996dc00b3b1347e43cee854341a2043563a449
Tyler Delmus <tylerdelmus@gmail.com>
2025-02-09 16:14:04 -0600

    Added AFCM Imports

---
+++ AOSharp.Common/Unmanaged/Imports/AFCM/AFCM.cs
@@ -0,0 +1,12 @@
+﻿using AOSharp.Common.GameData;
+using System;
+using System.Runtime.InteropServices;
+
+namespace AOSharp.Common.Unmanaged.Imports
+{
+    public class ACFM
+    {
+        [DllImport("AFCM.dll", EntryPoint = "?GetInstance@AFCM@@SAPAV1@XZ", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr GetInstance();
+    }
+}
